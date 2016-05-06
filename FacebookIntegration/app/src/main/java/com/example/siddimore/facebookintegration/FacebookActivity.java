5140b07ecb03fcd0fd5bc0e6febcc8620d651c7d
Siddharth More <siddimore@Siddharths-MacBook-Pro-2.local>
2016-05-06 12:08:55 -0700

    First Commit for FaceBook Integration

---
+++ FacebookIntegration/app/src/main/java/com/example/siddimore/facebookintegration/FacebookActivity.java
@@ -0,0 +1,13 @@
+package com.example.siddimore.facebookintegration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook);
+    }
+}
